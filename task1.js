20081ce1e5e730a20e72c61073065417952cf761
Chanchu1 <146652276+Chanchu1@users.noreply.github.com>
2025-06-20 10:47:29 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,49 @@
+// 1
+// console.log("hello world")
+// 2
+// console.log("your name")
+// 3
+// console.log("your name")
+// console.log("23")
+// // 4
+// string="name"
+// age=22
+// bollen=string=="name"&&age==22
+// console.log(string,age,bollen)
+// 5
+// name="chanchu"
+// age=31
+// console.log("your name:",name,"your age:",age)
+// 6
+// name="chanchu"
+// age=21
+// console.log(name,age)
+// 7
+// a=10
+// b=20
+// c=a+b
+// d=a-b
+// e=a*b
+// f=a/b
+// g=a%b
+// console.log(c,d,e,f,g)
+// 8
+// for(i=0;i<5;i++){
+//     a=""
+//     for(j=0;j<i;j++){
+//         a+="*"
+//     }
+// console.log(a)
+// }
+// 9
+// name="chanchu"
+// age=21
+// student=name
+// isstudent=student==name&&age==21
+// console.log(name,age,isstudent)
+// 10
+// name="chanchu"
+// age=21
+// dob="06/05/2004"
+// gender="male"
+// console.log(name,age,dob,gender)
